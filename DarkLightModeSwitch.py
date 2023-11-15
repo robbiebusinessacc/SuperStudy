f67fab3f0bfd89e9a23b0cf1fa596368b26ddd3e
Robbie Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-11-15 15:42:28 +0000

    formatting

--- DarkLightModeSwitch.py
+++ DarkLightModeSwitch.py
@@ -3,23 +3,39 @@ import subprocess
 
 def change_mode(mode):
     if mode == "dark":
-        subprocess.call(["osascript", "-e",
-                         'tell application "System Events" to tell appearance preferences to set dark mode to true'])
+        subprocess.call(
+            [
+                "osascript",
+                "-e",
+                'tell application "System Events" to tell appearance preferences to set dark mode to true',
+            ]
+        )
     elif mode == "light":
-        subprocess.call(["osascript", "-e",
-                         'tell application "System Events" to tell appearance preferences to set dark mode to false'])
+        subprocess.call(
+            [
+                "osascript",
+                "-e",
+                'tell application "System Events" to tell appearance preferences to set dark mode to false',
+            ]
+        )
     else:
         print("Invalid mode")
 
 
 def get_current_mode():
     result = subprocess.run(
-        ["osascript", "-e", 'tell application "System Events" to tell appearance preferences to get dark mode'],
-        stdout=subprocess.PIPE)
+        [
+            "osascript",
+            "-e",
+            'tell application "System Events" to tell appearance preferences to get dark mode',
+        ],
+        stdout=subprocess.PIPE,
+    )
     current_mode = result.stdout.strip().decode("utf-8")
     return current_mode
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     current_mode = get_current_mode()
     if current_mode == "true":
         change_mode("light")
