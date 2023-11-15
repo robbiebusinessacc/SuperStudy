f60c99d1de2884ad518a92bc6051dc1063c9f085
Robbie Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-11-15 15:41:25 +0000

    formatting

--- PomodoroTimer.py
+++ PomodoroTimer.py
@@ -1,10 +1,17 @@
-from AppKit import NSApplication, NSStatusBar, NSStatusItem, NSVariableStatusItemLength, NSMenu, NSMenuItem, NSTimer
+from AppKit import (
+    NSApplication,
+    NSStatusBar,
+    NSStatusItem,
+    NSVariableStatusItemLength,
+    NSMenu,
+    NSMenuItem,
+    NSTimer,
+)
 import objc
 import os
 
 
 class PomodoroTimer(object):
-
     def __init__(self):
         self.duration = 1500  # default duration of 25 minutes in seconds
         self.break_duration = 300  # default break duration of 5 minutes in seconds
@@ -13,45 +20,77 @@ class PomodoroTimer(object):
         self.timer = None
         self.break_timer = None
         self.statusbar = NSStatusBar.systemStatusBar()
-        self.statusitem = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
-        self.statusitem.setTitle_('Studying: {:02d}:{:02d}'.format(25, 0))
+        self.statusitem = self.statusbar.statusItemWithLength_(
+            NSVariableStatusItemLength
+        )
+        self.statusitem.setTitle_("Studying: {:02d}:{:02d}".format(25, 0))
         self.statusitem.setHighlightMode_(True)
-        self.statusitem.setToolTip_('Pomodoro Timer')
+        self.statusitem.setToolTip_("Pomodoro Timer")
 
         # create menu for the statusitem
         self.menu = NSMenu.alloc().init()
-        self.start_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Start", "startTimer:", "")
+        self.start_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+            "Start", "startTimer:", ""
+        )
         self.start_menu_item.setTarget_(self)
         self.menu.addItem_(self.start_menu_item)
-        self.stop_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Stop", "stopTimer:", "")
+        self.stop_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+            "Stop", "stopTimer:", ""
+        )
         self.stop_menu_item.setTarget_(self)
         self.menu.addItem_(self.stop_menu_item)
-        self.reset_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Reset", "resetTimer:", "")
+        self.reset_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+            "Reset", "resetTimer:", ""
+        )
         self.reset_menu_item.setTarget_(self)
         self.menu.addItem_(self.reset_menu_item)
         self.menu.addItem_(NSMenuItem.separatorItem())
         self.duration_menu = NSMenu.alloc().initWithTitle_("Duration")
-        self.duration_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Duration", None, "")
+        self.duration_menu_item = (
+            NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Duration", None, "")
+        )
         self.duration_menu_item.setSubmenu_(self.duration_menu)
         self.menu.addItem_(self.duration_menu_item)
         self.duration_menu_items = []
-        for duration in [10, 60 * 25, 60 * 30, 60 * 35, 60 * 40, 60 * 45, 60 * 50, 60 * 55, 60 * 60]:  # study durations
-            menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("{} minutes".format(duration // 60),
-                                                                               "setDuration:", "")
+        for duration in [
+            10,
+            60 * 25,
+            60 * 30,
+            60 * 35,
+            60 * 40,
+            60 * 45,
+            60 * 50,
+            60 * 55,
+            60 * 60,
+        ]:  # study durations
+            menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+                "{} minutes".format(duration // 60), "setDuration:", ""
+            )
             menu_item.setRepresentedObject_(duration)
             menu_item.setTarget_(self)
             self.duration_menu_items.append(menu_item)
             self.duration_menu.addItem_(menu_item)
         self.break_duration_menu = NSMenu.alloc().initWithTitle_("Break Duration")
-        self.break_duration_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Break Duration",
-                                                                                               None, "")
+        self.break_duration_menu_item = (
+            NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+                "Break Duration", None, ""
+            )
+        )
         self.break_duration_menu_item.setSubmenu_(self.break_duration_menu)
         self.menu.addItem_(self.break_duration_menu_item)
         self.break_duration_menu_items = []
-        for break_duration in [10, 60 * 5, 60 * 6, 60 * 7, 60 * 8, 60 * 9, 60 * 10]:  # break durations
+        for break_duration in [
+            10,
+            60 * 5,
+            60 * 6,
+            60 * 7,
+            60 * 8,
+            60 * 9,
+            60 * 10,
+        ]:  # break durations
             menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
-                "{} minutes".format(break_duration // 60),
-                "setBreakDuration:", "")
+                "{} minutes".format(break_duration // 60), "setBreakDuration:", ""
+            )
             menu_item.setRepresentedObject_(break_duration)
             menu_item.setTarget_(self)
             self.break_duration_menu_items.append(menu_item)
@@ -60,9 +99,13 @@ class PomodoroTimer(object):
 
     def send_notification(self, message):
         title = "Pomodoro Timer"
-        os.system("""
+        os.system(
+            """
                       osascript -e 'display notification "{}" with title "{}"'
-                      """.format(message, title))
+                      """.format(
+                message, title
+            )
+        )
 
     def startTimer_(self, sender):
         if self.break_timer and self.break_remaining_time > 0:
@@ -77,9 +120,9 @@ class PomodoroTimer(object):
             # update the status item
             self.update_status_item()
             # start the timer
-            self.timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(1.0, self,
-                                                                                                  'update_time', None,
-                                                                                                  True)
+            self.timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+                1.0, self, "update_time", None, True
+            )
 
     def startBreak_(self, sender):
         # invalidate any previous timer
@@ -91,9 +134,11 @@ class PomodoroTimer(object):
         # update the status item
         self.update_status_item()
         # start the timer
-        self.break_timer = NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(1.0, self,
-                                                                                                    'update_break_time',
-                                                                                                    None, True)
+        self.break_timer = (
+            NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+                1.0, self, "update_break_time", None, True
+            )
+        )
 
     def stopTimer_(self, sender):
         if self.timer:
@@ -154,13 +199,16 @@ class PomodoroTimer(object):
         if self.remaining_time > 0:
             minutes = self.remaining_time // 60
             seconds = self.remaining_time % 60
-            self.statusitem.setTitle_('Studying: {:02d}:{:02d}'.format(minutes, seconds))
+            self.statusitem.setTitle_(
+                "Studying: {:02d}:{:02d}".format(minutes, seconds)
+            )
         else:
             minutes = self.break_remaining_time // 60
             seconds = self.break_remaining_time % 60
-            self.statusitem.setTitle_('Break: {:02d}:{:02d}'.format(minutes, seconds))
+            self.statusitem.setTitle_("Break: {:02d}:{:02d}".format(minutes, seconds))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = NSApplication.sharedApplication()
     pomodoro_timer = PomodoroTimer()
     app.setDelegate_(pomodoro_timer)
