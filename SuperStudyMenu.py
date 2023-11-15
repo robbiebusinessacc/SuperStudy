07a141ae9a1d797505c76b16a0a79aa0060688da
Robbie Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-11-15 15:41:10 +0000

    formatting

--- SuperStudyMenu.py
+++ SuperStudyMenu.py
@@ -1,4 +1,12 @@
-from AppKit import NSApplication, NSStatusBar, NSStatusItem, NSVariableStatusItemLength, NSMenu, NSMenuItem, NSTimer
+from AppKit import (
+    NSApplication,
+    NSStatusBar,
+    NSStatusItem,
+    NSVariableStatusItemLength,
+    NSMenu,
+    NSMenuItem,
+    NSTimer,
+)
 import subprocess
 import objc
 import os
@@ -12,24 +20,38 @@ from MusicPlayer import open_spotify
 # Import code from DarkLightModeSwitch.py
 from DarkLightModeSwitch import change_mode, get_current_mode
 
-class MacOSMenuBar(object):
 
+class MacOSMenuBar(object):
     def __init__(self):
         self.statusbar = NSStatusBar.systemStatusBar()
-        self.statusitem = self.statusbar.statusItemWithLength_(NSVariableStatusItemLength)
-        self.statusitem.setTitle_('Placeholder Icon')
+        self.statusitem = self.statusbar.statusItemWithLength_(
+            NSVariableStatusItemLength
+        )
+        self.statusitem.setTitle_("Placeholder Icon")
         self.statusitem.setHighlightMode_(True)
-        self.statusitem.setToolTip_('MacOS Menu Bar')
+        self.statusitem.setToolTip_("MacOS Menu Bar")
 
         # create menu for the statusitem
         self.menu = NSMenu.alloc().init()
-        self.pomodoro_timer_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Pomodoro Timer", "startPomodoroTimer:", "")
+        self.pomodoro_timer_menu_item = (
+            NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+                "Pomodoro Timer", "startPomodoroTimer:", ""
+            )
+        )
         self.pomodoro_timer_menu_item.setTarget_(self)
         self.menu.addItem_(self.pomodoro_timer_menu_item)
-        self.music_player_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Music Player", "startMusicPlayer:", "")
+        self.music_player_menu_item = (
+            NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+                "Music Player", "startMusicPlayer:", ""
+            )
+        )
         self.music_player_menu_item.setTarget_(self)
         self.menu.addItem_(self.music_player_menu_item)
-        self.dark_light_mode_switch_menu_item = NSMenuItem.alloc().initWithTitle_action_keyEquivalent_("Dark/Light Mode Switch", "startDarkLightModeSwitch:", "")
+        self.dark_light_mode_switch_menu_item = (
+            NSMenuItem.alloc().initWithTitle_action_keyEquivalent_(
+                "Dark/Light Mode Switch", "startDarkLightModeSwitch:", ""
+            )
+        )
         self.dark_light_mode_switch_menu_item.setTarget_(self)
         self.menu.addItem_(self.dark_light_mode_switch_menu_item)
         self.statusitem.setMenu_(self.menu)
@@ -51,7 +73,9 @@ class MacOSMenuBar(object):
             change_mode("dark")
         else:
             print("Could not determine current mode")
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     app = NSApplication.sharedApplication()
     macos_menu_bar = MacOSMenuBar()
     app.setDelegate_(macos_menu_bar)
